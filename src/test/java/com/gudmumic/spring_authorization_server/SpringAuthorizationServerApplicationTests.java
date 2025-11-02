1adb066593f932f82e4c6bbc74719e47c89b7416
Michael Nielsen <gudmundseth.dk@gmail.com>
2025-11-02 22:14:55 +0100

    create project from spring initializr

---
+++ src/test/java/com/gudmumic/spring_authorization_server/SpringAuthorizationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gudmumic.spring_authorization_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthorizationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
